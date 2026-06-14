fc9b2e675c653ca9ce55af673cb39b70319bc65d
vijaymahes9080 <vijaymahes9080@users.noreply.github.com>
2026-06-14 21:25:45 +0530

    Fix APP_URL in phpunit.xml to prevent 404 route mismatch in testing environment

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,6 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->withoutExceptionHandling();
         $response = $this->get('/');
 
         $response->assertStatus(200);
