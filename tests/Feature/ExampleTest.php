6742a9daaabaea00fc36fa5bb44b6e18e739f9bd
vijaymahes9080 <vijaymahes9080@users.noreply.github.com>
2026-06-14 21:24:17 +0530

    Enable detailed exception tracing in ExampleTest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        $this->withoutExceptionHandling();
         $response = $this->get('/');
 
         $response->assertStatus(200);
